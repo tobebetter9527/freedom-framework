93aaa1c6cecb36f9767a8c34b8c85ad5a914a130
tobebetter9527 <30531224+tobebetter9527@users.noreply.github.com>
2021-05-08 00:46:20 +0800

    DB_CLOSE_DELAY=-1,keep the database open

--- freedom-common/src/main/java/com/freedom/common/entity/UserDO.java
+++ freedom-common/src/main/java/com/freedom/common/entity/UserDO.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.freedom.common.model.BaseDO;
+import java.util.Objects;
 import lombok.Data;
 
 /**
